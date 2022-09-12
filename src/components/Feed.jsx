c4ad604a0872125c34f318ef91850fb876b2c0de
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-09-12 15:10:27 +0530

    nav and feed

---
+++ src/components/Feed.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      ></Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
+const SearchBar = () => {
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 }, //margin right only on small devices
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton type="submit" sx={{ color: "red", p: "10px" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
