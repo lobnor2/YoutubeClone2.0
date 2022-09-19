63c3f737bd0d296d4b99583a1c8030015251abfb
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-09-20 01:20:16 +0530

    working on search feed

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import Videos from "./Videos";
+import ChannelCard from "./ChannelCard";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=&{id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=&{id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            // background: "linear-gradient(to right, #f857a6, #ff5858)",
+            zIndex: 10,
+            height: "300px",
+            background: "linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-100px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
