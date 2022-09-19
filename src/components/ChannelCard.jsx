8041268bb1e6ac48699c3ae27399f11fd9b28fff
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-09-20 01:20:16 +0530

    working on search feed

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
+const ChannelCard = ({ channelDetail, marginTop }) => {
+  return (
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRaduis: "20px",
+        // border: "1px solid white",
+        width: { xs: "467px", sm: " 350px", md: "280px" },
+        height: "305px",
+        margin: "auto",
+        marginTop: marginTop,
+        // display: "flex",
+        // justifyContent: "center",
+        // alignItems: "center",
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            // textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thubmnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              height: "180px",
+              width: "180px",
+              mb: "20px",
+            }}
+          />
+          <Typography
+            variant="h6"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 19, color: "gray", ml: "5px" }} />
+          </Typography>
+        </CardContent>
+      </Link>
+      ChannelCard
+    </Box>
+  );
 };
 
 export default ChannelCard;
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, []);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        mb={2}
+        sx={{
+          color: "white",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        Searcj Results for: 
+        <span style={{ color: "#F31503", marginLeft: "10px" }}> Videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
-export default SearchFeed;
+export default Feed;
