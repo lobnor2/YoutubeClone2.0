6138761b1b279d89ec109ada4be3e5754a78f131
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-08 23:27:48 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppContext } from "./context/contextApi";
 import Header from "./components/Header";
+import Feed from "./components/Feed";
+import SearchResult from "./components/SearchResult";
+import VideoDetails from "./components/VideoDetails";
+
 const App = () => {
   return (
     <AppContext>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h2>this is heading 2</h2>
+      <Router>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route
+              path="/searchresult/:searchquery"
+              element={<SearchResult />}
+            />
+            <Route path="/videodetails/:id" element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </Router>
     </AppContext>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,6 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <h1>hello world</h1>
   </React.StrictMode>
 );
