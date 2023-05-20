f50018139bf4c7059589dd47fea65df1508d2a3e
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-20 23:39:32 +0530

    add a leftnav component in feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import LeftNavMenuItem from "./LeftNavMenuItem";
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)] border border-red-700">
+      <LeftNav />
+    </div>
+  );
 };
 
 export default Feed;
