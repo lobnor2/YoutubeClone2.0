2beb80b75552f4bc5355f1a39e38458cdea8ffc4
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-21 23:45:30 +0530

    add a click handler and update selectCategory to selectCategories

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -5,8 +5,21 @@ import { categories } from "../utils/constants";
 import { Context } from "../context/contextApi";
 
 const LeftNav = () => {
-  const { selectedCategory, setSelectedCategory, mobileMenu } =
+  const { selectCategories, setSelectCategories, mobileMenu } =
     useContext(Context);
+  const navigate = useNavigate();
+  const clickHandler = (name, type) => {
+    switch (type) {
+      case "category":
+        return setSelectCategories(name);
+      case "home":
+        return setSelectCategories(name);
+      case "menu":
+        return false;
+      default:
+        break;
+    }
+  };
   return (
     <div className="md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all border border-blue-500 text-white">
       <div className="flex px-5 flex-col">
@@ -15,9 +28,12 @@ const LeftNav = () => {
             <LeftNavMenuItem
               text={item.type === "home" ? "Home" : item.name}
               icon={item.icon}
-              action={() => {}}
+              action={() => {
+                clickHandler(item.name, item.type);
+                navigate("/");
+              }}
               className={`${
-                selectedCategory === item.name ? "bg-white/[0.15]" : ""
+                selectCategories === item.name ? "bg-white/[0.15]" : ""
               }`}
             />
             {item.divider && <hr className="my-5 border-white/[0.2]" />}
