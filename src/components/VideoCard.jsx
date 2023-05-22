af2a6785b3d3677e7d54f231f95d239d7d626eaf
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-23 01:32:31 +0530

    add a thumbnail, avatar, title, views and time

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -15,6 +15,36 @@ const VideoCard = ({ video }) => {
           />
           {video?.lengthSeconds && <VideoLength time={video?.lengthSeconds} />}
         </div>
+        <div className="flex text-white mt-3">
+          <div className="flex items-start">
+            <div className="flex h-9 w-9 rounded-full overflow-hidden border border-white">
+              <img
+                src={video?.author?.avatar[0]?.url}
+                alt=""
+                className="h-full w-full object-cover"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col ml-3 overflow-hidden">
+            <span className="text-sm font-bold truncate">{video?.title}</span>
+            <span className="text-[12px] font-semibold mt-1 text-white/[0.7] flex items-center">
+              {video?.author?.title}
+              {/* {video?.author?.badges[0].type === "VERIFIED_CHANNEL" && (
+                <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] ml-1" />
+              )} */}
+            </span>
+            <div className="flex text-[12px] font-semibold text-white/[0.7] truncate overflow-hidden">
+              <span>{`${abbreviateNumber(
+                video?.author?.stats?.views,
+                2
+              )} views`}</span>
+              <span className="flex text-[24px] leading-none font-bold text-white/[0.7] relative top-[-10px] mx-1">
+                .
+              </span>
+              <span className="truncate">{video?.publishedTimeText}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </Link>
   );
