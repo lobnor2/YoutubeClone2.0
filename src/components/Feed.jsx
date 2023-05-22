0992cfe259a66e82a7456d338c19a675d8f76537
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-22 23:58:23 +0530

    add a thumbnail and time on image

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,27 @@
 import React, { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import LeftNavMenuItem from "./LeftNavMenuItem";
+
 import { Context } from "../context/contextApi";
 import LeftNav from "./LeftNav";
+import VideoCard from "./VideoCard";
 const Feed = () => {
+  const { loading, searchResults } = useContext(Context);
+  useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
+  }, []);
   return (
     <div className="flex flex-row h-[calc(100%-56px)] border border-red-700">
       <LeftNav />
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+          {!loading &&
+            searchResults.map((item) => {
+              if (item.type !== "video") return false;
+              return (
+                <VideoCard key={item?.video?.videoId} video={item?.video} />
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+import { abbreviateNumber } from "js-abbreviation-number";
+import { Link } from "react-router-dom";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import VideoLength from "../shared/videoLength";
+const VideoCard = ({ video }) => {
+  return (
+    <Link to={`/video/${video?.videoId}`}>
+      <div className="flex flex-col mb-8">
+        <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden">
+          <img
+            src={video?.thumbnails?.[0]?.url}
+            alt=""
+            className="h-full w-full"
+          />
+          {video?.lengthSeconds && <VideoLength time={video?.lengthSeconds} />}
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default VideoCard;
