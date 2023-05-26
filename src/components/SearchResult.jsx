e646bf5eed52c4834b58c87240a067becec510f4
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-27 00:16:19 +0530

    Add a left nav and search result video card components

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -11,17 +11,31 @@ const SearchResult = () => {
   const { searchquery } = useParams();
   const { setLoading } = useContext(Context);
   useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
     fetchSearchResults(searchquery);
   }, [searchquery]);
   const fetchSearchResults = (searchquery) => {
     setLoading(true);
     fetchDataFromApi(`search/?q=${searchquery}`).then((res) => {
       console.log(res);
-      setResult(res);
+      setResult(res?.contents);
       setLoading(false);
     });
   };
-  return <div>SearchResult</div>;
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+      <div className="grow w-[clac(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 gap-2 p-5">
+          {result?.map((item) => {
+            if (item?.type !== "video") return false;
+            let video = item?.video;
+            return <SearchResultVideoCard key={video?.videoId} video={video} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchResult;
