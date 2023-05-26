407f34912a8a4dd290090c3e8a253af10cfbf431
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-26 23:46:55 +0530

    Add a function to call api

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+
+import { fetchDataFromApi } from "../utils/api";
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
+import SearchResultVideoCard from "./SearchResultVideoCard";
 
 const SearchResult = () => {
+  const [result, setResult] = useState();
+  const { searchquery } = useParams();
+  const { setLoading } = useContext(Context);
+  useEffect(() => {
+    fetchSearchResults(searchquery);
+  }, [searchquery]);
+  const fetchSearchResults = (searchquery) => {
+    setLoading(true);
+    fetchDataFromApi(`search/?q=${searchquery}`).then((res) => {
+      console.log(res);
+      setResult(res);
+      setLoading(false);
+    });
+  };
   return <div>SearchResult</div>;
 };
 
