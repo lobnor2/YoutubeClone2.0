05d97ec9e5a72782bec8dba399d9e06c121e4ff9
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-28 00:58:16 +0530

    Update height for video details

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -8,9 +8,11 @@ import { abbreviateNumber } from "js-abbreviation-number";
 import { fetchDataFromApi } from "../utils/api";
 import { Context } from "../context/contextApi";
 import SuggestionVideoCard from "./SuggestionVideoCard";
+import { MdCommentsDisabled } from "react-icons/md";
 const VideoDetails = () => {
   const [video, setVideo] = useState([]);
   const [relatedVideos, setRelatedVideos] = useState([]);
+  const [comments, setComments] = useState([]);
   const { id } = useParams();
   const { setLoading } = useContext(Context);
   //call useffect when id change in url
@@ -18,12 +20,13 @@ const VideoDetails = () => {
     document.getElementById("root").classList.add("custom-h");
     fetchVideoDetails();
     fetchRelatedVideos();
+    fetchVideoComments();
   }, [id]);
 
   const fetchVideoDetails = () => {
     setLoading(true);
     fetchDataFromApi(`video/details/?id=${id}`).then((res) => {
-      console.log(res);
+      // console.log(res);
       setVideo(res);
       setLoading(false);
     });
@@ -31,14 +34,21 @@ const VideoDetails = () => {
   const fetchRelatedVideos = () => {
     setLoading(true);
     fetchDataFromApi(`video/related-contents/?id=${id}`).then((res) => {
-      console.log(res);
+      // console.log(res);
       setRelatedVideos(res);
+      setLoading(false);
+    });
+  };
+  const fetchVideoComments = () => {
+    setLoading(true);
+    fetchDataFromApi(`video/comments/?id=${id}`).then((res) => {
+      setComments(res);
       setLoading(false);
     });
   };
 
   return (
-    <div className="flex justify-center flex-row h-[calc(100%-56px)] bg-black border border-white p-1">
+    <div className="flex justify-center flex-row  bg-black border border-white p-1">
       <div className="w-full max-w-[1280px] flex flex-col lg:flex-row border border-green-600 p-1">
         <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc(100%-400px)] px-4 py-3 lg:py-6 overflow-y-auto border border-blue-700 p-1">
           <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0 border border-yellow-500">
@@ -78,15 +88,16 @@ const VideoDetails = () => {
               </div>
             </div>
             <div className="flex text-white mt-4 md:mt-0">
-              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
+              <div className="flex items-center justify-center h-11 px-5 rounded-3xl bg-white/[0.15]">
                 <AiOutlineLike className="text-xl text-white mr-2" />
                 <span>{`${abbreviateNumber(video?.stats?.likes)} Likes`}</span>
               </div>
-              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15] ml-4">
+              <div className="flex items-center justify-center h-11 px-5 rounded-3xl bg-white/[0.15] ml-4">
                 <span>{`${abbreviateNumber(video?.stats?.views)} Views`}</span>
               </div>
             </div>
           </div>
+          <div className="flex border border-white w-full h-[500px] mt-4"></div>
         </div>
         <div className="flex flex-col py-6 px-4 overflow-y-auto lg:w-[350px] xl:w-[400px]">
           {relatedVideos?.contents?.map((item, index) => {
