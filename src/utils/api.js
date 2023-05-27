930714725fa59aaa9d672c80a7b7451f1761f3d1
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-27 21:21:41 +0530

    fix .env file

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,12 +6,7 @@ const options = {
     gl: "US",
   },
   headers: {
-    // f12e68dde8msh7f65513efc51dc6p1ffff9jsn2464e507fe2e
-    // 51de22aca4msh67ff75d067c1bc1p1a8a20jsnb40e342a9b2f
-    // "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-
-    // "X-RapidAPI-Key": "f12e68dde8msh7f65513efc51dc6p1ffff9jsn2464e507fe2e",
-    // "X-RapidAPI-Key": "51de22aca4msh67ff75d067c1bc1p1a8a20jsnb40e342a9b2f",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
