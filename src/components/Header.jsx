ea2a64514fef9f60c76326d4a188ba9de0e6880a
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-05-27 20:52:55 +0530

    Update profile pic in mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -87,13 +87,13 @@ const Header = () => {
           <div className="flex items-center justify-center ml-2 h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
             <FiBell className="text-white text-xl cursor-pointer" />
           </div>
-          <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
-            <img
-              src="https://xsgames.co/randomusers/avatar.php?g=male"
-              alt="profile pic"
-              className="cursor-pointer"
-            />
-          </div>
+        </div>
+        <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
+          <img
+            src="https://xsgames.co/randomusers/avatar.php?g=male"
+            alt="profile pic"
+            className="cursor-pointer"
+          />
         </div>
       </div>
     </div>
